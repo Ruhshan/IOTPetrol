d29a3ae34f982e76dbdb91151a77f79e7d8e903f
Ruhshan <ruhshan.ahmed@gmail.com>
2018-05-20 12:34:56 +0600

    using postgres

--- project/settings.py
+++ project/settings.py
@@ -81,10 +81,17 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'petrol_db',
+        'USER': 'postgres',
     }
 }
 
